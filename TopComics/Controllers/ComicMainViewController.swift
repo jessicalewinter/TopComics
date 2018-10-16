045f370cea570710aa198174840f66cad20a1423
jessicalewinter <jessicalewinter@gmail.com>
2018-10-16 11:17:57 -0300

    Working with  json parsing

---
+++ TopComics/Controllers/ComicMainViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ComicMainViewController.swift
+//  TopComics
+//
+//  Created by Jessica Lewinter on 16/10/18.
+//  Copyright © 2018 Jessica Lewinter. All rights reserved.
+//
+
+import UIKit
+
+class ComicMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        parseJSON()
+    }
+    
+    func parseJSON(){
+        let apiKey = "2a6cf3cd37e3e9daa367f78efc63fbddee943b92"
+        var url = URL(string: "https://comicvine.gamespot.com/api/characters/?api_key=\(apiKey)&format=json&limit=2")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let dataResponse = data, error == nil else{
+                print("Error: \(error?.localizedDescription)" ?? "Error to fetch url")
+                return
+            }
+            do{
+                let jsonResponse =  try JSONSerialization.jsonObject(with: dataResponse, options: [])
+                
+                DispatchQueue.main.async {
+                    print(jsonResponse)
+                }
+                
+            } catch{
+                DispatchQueue.main.async {
+                    print("Unable to parse json in \(url)")
+                }
+                
+            }
+        }
+        DispatchQueue.global(qos: .background).async {
+            task.resume()
+        }
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TopComics/Models/Comic.swift
@@ -0,0 +1,13 @@
+//
+//  Comic.swift
+//  TopComics
+//
+//  Created by Jessica Lewinter on 16/10/18.
+//  Copyright © 2018 Jessica Lewinter. All rights reserved.
+//
+
+import UIKit
+
+class Comic: NSObject {
+
+}
